f1c88b5e2d73662a59f785d68488b567972b7029
yevhenii <yevhenii.filonenko@aline-consulting.com>
2016-09-12 10:43:49 +0300

    implement bold & improve cellRenderer.

--- src/components/Grid.Cell.jsx
+++ src/components/Grid.Cell.jsx
@@ -1,19 +1,74 @@
 import React from 'react';
+import bind from 'autobind-decorator'
+import _ from 'lodash'
 
 export default class GridCell extends React.Component {
     static propTypes = {
         params: React.PropTypes.object
     };
 
+    componentWillMount() {
+        this.setState(
+            _.pick(
+                this.props.params.value,
+                ['align', 'bold', 'italic', 'fontSize']
+            )
+        )
+    }
+
+    @bind
+    handleCellClick() {
+        this.props.params.handleCellClick && this.props.params.handleCellClick(
+            this
+        )
+    }
+
+    @bind
+    setAlign(align) {
+        this.setState({
+            align
+        })
+    }
+
+    @bind
+    setBold(bold) {
+        this.setState({
+            bold
+        })
+    }
+
+    @bind
+    setItalic(italic) {
+        this.setState({
+            italic
+        })
+    }
+
+    @bind
+    setFontSize(fontSize) {
+        this.setState({
+            fontSize
+        })
+    }
+
     render() {
+        //console.log('re-render')
         if (this.props.params.value) {
-            const {text, align} = this.props.params.value;
             return (
-                <div style={{textAlign: align}}>
-                    {text}
+                <div
+                    style={{
+                        textAlign: this.state.align,
+                        fontWeight: this.state.bold && 'bold',
+                        fontStyle: this.state.italic && 'italic',
+                        fontSize: this.state.fontSize
+                    }}
+                    onClick={this.handleCellClick}>
+                    {this.props.params.value.text}
                 </div>
             );
         }
+
         return null;
+
     }
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import {AgGridReact, reactCellRendererFactory} from 'ag-grid-react'
 import GridCell from './Grid.Cell'
 import request from 'superagent'
-import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton'
+import SelectField from 'material-ui/SelectField'
+import MenuItem from 'material-ui/MenuItem'
 import _ from 'lodash'
+import bind from 'autobind-decorator'
 
 const COLUMN_NUM = 4;
 
@@ -11,12 +14,15 @@ export default class Grid extends React.Component {
     state = {
         data: [],
         headers: [],
-        cell: null
+        cellInstance: null
     };
 
     componentWillMount() {
 
         const cellRenderer = reactCellRendererFactory(GridCell, this);
+        const cellRendererParams  = {
+            handleCellClick: this.setCellInstance,
+        };
 
         request.get('/api/grid/').end((err, res) => {
             this.setState({
@@ -26,13 +32,15 @@ export default class Grid extends React.Component {
             })
         });
 
-        const headers = [{ headerName: "#", valueGetter: "node.id / 1 + 1", exclude: true }];
+        const headers = [{ headerName: "#", valueGetter: "node.id / 1 + 1" }];
 
         _.times(COLUMN_NUM, i => {
             headers.push({
                 headerName: i,
                 field: i + '',
                 cellRenderer,
+                cellRendererParams
+
             })
         });
 
@@ -40,55 +48,105 @@ export default class Grid extends React.Component {
 
     }
 
-    handleAlign(align = 'left') {
-        if (this.state.cell) {
+    @bind
+    asyncPatchEntity(patchObj, clb) {
+        if (this.state.cellInstance) {
+            const id = this.state.cellInstance.props.params.value.id;
             request
-                .patch(`/api/grid/${this.state.cell.data.id}`)
-                .send({ align })
+                .patch(`/api/grid/${id}`)
+                .send(patchObj)
                 .end(err => {
-                    !err && (this.state.cell.target.style.textAlign = align)
+                    !err && clb(patchObj)
                 })
         }
     }
 
-    handleCellClick(params) {
-        this.state.cell && (this.state.cell.target.style.background = 'inherit');
-        if (!params.colDef.exclude && params.value) {
-            const target = params.event.target;
-            target.style.background = 'gray';
-            this.setState({
-                cell: {
-                    data: params.value,
-                    target
-                }
-            })
-        } else { this.setState({ cell: null }) }
+    @bind
+    handleAlign(align) {
+        this.asyncPatchEntity({ align },
+            obj => this.state.cellInstance.setAlign(
+                obj.align
+            )
+        );
+    }
 
+    @bind
+    handleBold() {
+        this.asyncPatchEntity({ bold: !this.state.cellInstance.state.bold },
+            obj => this.state.cellInstance.setBold(
+                obj.bold
+            )
+        );
+    }
+
+    @bind
+    handleItalic() {
+        this.asyncPatchEntity({ italic: !this.state.cellInstance.state.italic },
+            obj => this.state.cellInstance.setItalic(
+                obj.italic
+            )
+        );
+    }
+
+    @bind
+    handleFontSize(e, i, fontSize) {
+        this.asyncPatchEntity({ fontSize },
+            obj => this.state.cellInstance.setFontSize(
+                obj.fontSize
+            )
+        );
+    }
+
+    @bind
+    setCellInstance(cellInstance) {
+        this.setState({
+            cellInstance
+        })
     }
 
     render() {
         return <div className="ag-fresh" style={{ height: "50vh" }}>
 
             <RaisedButton
-                disabled={!this.state.cell}
-                onTouchTap={_.wrap('left', this.handleAlign.bind(this))}
+                disabled={!this.state.cellInstance}
+                onTouchTap={_.wrap('left', this.handleAlign)}
                 label="Left"
             />
 
             <RaisedButton
-                disabled={!this.state.cell}
-                onTouchTap={_.wrap('center', this.handleAlign.bind(this))}
+                disabled={!this.state.cellInstance}
+                onTouchTap={_.wrap('center', this.handleAlign)}
                 label="Center"
             />
 
             <RaisedButton
-                disabled={!this.state.cell}
-                onTouchTap={_.wrap('right', this.handleAlign.bind(this))}
+                disabled={!this.state.cellInstance}
+                onTouchTap={_.wrap('right', this.handleAlign)}
                 label="Right"
             />
 
+            <RaisedButton
+                disabled={!this.state.cellInstance}
+                onTouchTap={this.handleBold}
+                label="B"
+            />
+
+            <RaisedButton
+                disabled={!this.state.cellInstance}
+                onTouchTap={this.handleItalic}
+                label="I"
+            />
+
+            Font Size: <SelectField
+                disabled={!this.state.cellInstance}
+                value={this.state.cellInstance && this.state.cellInstance.state.fontSize}
+                onChange={this.handleFontSize} maxHeight={200}>
+                <MenuItem value={12} primaryText={12} />
+                <MenuItem value={14} primaryText={14} />
+                <MenuItem value={16} primaryText={16} />
+            </SelectField>
+
             <AgGridReact
-                onCellClicked={this.handleCellClick.bind(this)}
                 columnDefs={this.state.headers}
                 rowData={this.state.data}
             />
